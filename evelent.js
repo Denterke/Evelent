b15c621cbb00025df7ac7a782d6759e6ba603b11
Denter <ADGFQ121519@gmail.com>
2015-11-06 08:47:16 +0000

    Initial Commit

---
+++ evelent.js
@@ -0,0 +1,21 @@
+var Hapi = require('hapi'); //require с hapi
+
+// Create a server with a host and port
+var server = new Hapi.Server();
+server.connection({
+    host: '31.131.24.188',
+    port: 8080
+});
+
+server.route({
+    method: 'GET',
+    path:'/evelent',
+    handler: function (request, reply) {
+      reply('WORK SERVER!');
+    }
+});
+
+// Start the server
+server.start(function () {
+    console.log('Server running at:', server.info.uri);
+});
